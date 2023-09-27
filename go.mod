585483c5b555aae0b18e6078645137e2ecf165af
Hoang Nguyen Huy <hoang.nguyenhuy@optimizely.com>
2023-09-27 11:03:44 +0700

    Doesn't seem like we need to specify credentials for the provider

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.20
 
 require (
 	github.com/pulumi/pulumi-aws-native/sdk v0.77.0
-	github.com/pulumi/pulumi/sdk/v3 v3.85.0
+	github.com/pulumi/pulumi/sdk/v3 v3.86.0
 )
 
 require (
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/pulumi/pulumi-aws-native/sdk/go/aws"
 	"github.com/pulumi/pulumi-aws-native/sdk/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -11,10 +9,7 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		awsProvider, err := aws.NewProvider(ctx, "staging", &aws.ProviderArgs{
-			Region:               pulumi.String("us-east-1"),
-			AccessKey:            pulumi.String(os.Getenv("AWS_ACCESS_KEY_ID")),
-			SecretKey:            pulumi.String(os.Getenv("AWS_SECRET_ACCESS_KEY")),
-			SkipMetadataApiCheck: pulumi.Bool(true),
+			Region: pulumi.String("us-east-1"),
 		})
 		if err != nil {
 			return err
